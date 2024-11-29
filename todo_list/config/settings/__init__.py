f9960578d1812e1509f3d533eb64649235f6c9ee
dedida <136377581+dedida26@users.noreply.github.com>
2024-11-29 17:03:57 +0300

    Delete todo_list directory

--- todo_list/config/settings/__init__.py
+++
@@ -1,3 +0,0 @@
-from config.settings.django import *
-from config.settings.logging import *
-from config.settings.drf import *
--- todo_list/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Folder, Page, Task, FolderPermission, PagePermission, TaskPermission
-
-admin.site.register(Folder)
-admin.site.register(Page)
-admin.site.register(Task)
-admin.site.register(FolderPermission)
-admin.site.register(PagePermission)
-admin.site.register(TaskPermission)
