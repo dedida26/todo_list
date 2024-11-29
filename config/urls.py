ef67227453e0fc70db2b62bc0ad1513078d0ecdc
dedida <136377581+dedida26@users.noreply.github.com>
2024-11-29 17:05:29 +0300

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import include, path, re_path
+from rest_framework import routers
+from todo import views
+from django.contrib import admin
+from todo.views import FolderPermissionViewSet, PagePermissionViewSet, TaskPermissionViewSet
+
+router = routers.DefaultRouter()
+router.register(r'folderperm', FolderPermissionViewSet)
+router.register(r'pageperm', PagePermissionViewSet)
+router.register(r'taskperm', TaskPermissionViewSet)
+router.register(r'folders', views.FolderViewSet)
+router.register(r'pages', views.PageViewSet)
+router.register(r'tasks', views.TaskViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v3/todo-auth/', include('rest_framework.urls')),
+    path('api/v3/', include(router.urls)),
+    path('api/v3/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v3/pages/<int:pk>/', views.PageViewSet.as_view({'get': 'retrieve'}), name='page-detail'),
+    path('api/v3/folders/<int:pk>/', views.FolderViewSet.as_view({'get': 'retrieve'}), name='folder-detail'),
+]
---
+++ todo/auth_log.py
@@ -0,0 +1,27 @@
+import logging
+from django.utils import timezone
+
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from django.http import HttpRequest
+
+# Настраиваем логгер для событий аутентификации
+logger = logging.getLogger('auth_log')
+
+
+# Обработчик сигнала входа пользователя. Логирует информацию о входе
+@receiver(user_logged_in)
+def log_login(sender, request: HttpRequest, user, **kwargs):
+    user_agent = request.META.get('HTTP_USER_AGENT', 'Unknown')
+    ip_address = request.META.get('REMOTE_ADDR', 'Unknown')
+    time = timezone.now()
+
+    log_message = (
+        f'\n'
+        f"Пользователь - {user.id} ({user.username}) вошел в систему.\n"
+        f"IP-адрес: {ip_address}\n"
+        f"User-Agent: {user_agent}\n"
+        f"Время: {time}\n"
+    )
+
+    logger.info(log_message)
